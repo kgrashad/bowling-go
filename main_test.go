e1558f0aad6f55b7203ccb039363154c81788be1
Khaled Rashad <kras@live.com>
2021-05-12 22:40:10 +0400

    Refactor test cases

--- main_test.go
+++ main_test.go
@@ -5,27 +5,28 @@ import "testing"
 func TestBowling(t *testing.T) {
 
 	t.Run("Zero rolls should return zero score", func(t *testing.T) {
-		game := Game{}
-
-		game.Roll(0)
-
-		got := game.Score()
+		got := rollAndGetScore(0)
 		want := 0
 
 		assertScore(want, got, t)
 	})
 
 	t.Run("Two rolls should return sum of pins", func(t *testing.T) {
-		game := Game{}
-
-		game.Roll(2)
-		game.Roll(3)
-
-		got := game.Score()
+		got := rollAndGetScore(2, 3)
 		want := 5
 
 		assertScore(want, got, t)
 	})
+}
+
+func rollAndGetScore(rolls ...int) (score int) {
+	game := Game{}
+
+	for _, roll := range rolls {
+		game.Roll(roll)
+	}
+
+	return game.Score()
 }
 
 func assertScore(want, got int, t *testing.T) {
